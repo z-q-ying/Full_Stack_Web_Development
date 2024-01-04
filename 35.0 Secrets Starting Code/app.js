ba0524d22603a125a96644fb949315112f651c31
z-q-ying <z.qiuying@outlook.com>
2024-01-04 15:40:16 -0800

    Level 1 authentication: Save username and password directly in MongoDB

--- 35.0 Secrets Starting Code/app.js
+++ 35.0 Secrets Starting Code/app.js
@@ -1,1 +1,79 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true })); // 解析HTML表单数据
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(() => {
+      res.render("secrets");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // check through the DB
+  User.findOne({ email: username }).then(
+    (foundUser) => {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        } else {
+          res.send("Wrong password");
+        }
+      } else {
+        res.send("No such user");
+      }
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
