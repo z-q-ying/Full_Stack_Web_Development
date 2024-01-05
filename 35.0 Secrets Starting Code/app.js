29dcf4545c4036f50ec45c37d07023e79a1a54d9
z-q-ying <z.qiuying@outlook.com>
2024-01-04 16:27:11 -0800

    Using Environment Variables to Keep Secrets Safe

--- 35.0 Secrets Starting Code/app.js
+++ 35.0 Secrets Starting Code/app.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -22,8 +22,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = new mongoose.model("User", userSchema);
 
