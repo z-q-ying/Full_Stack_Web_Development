4aabc9b28c05b3619a078a1cfa2bcff140be82dd
z-q-ying <z.qiuying@outlook.com>
2024-01-05 14:38:15 -0800

    Level 4 - Salting and Hashing Passwords with bcrypt

--- 35.0 Secrets Starting Code/app.js
+++ 35.0 Secrets Starting Code/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 8; // rounds=8 : ~40 hashes/sec
 
 const app = express();
 const port = 3000;
@@ -37,34 +38,37 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-
-  newUser
-    .save()
-    .then(() => {
-      res.render("secrets");
-    })
-    .catch((err) => {
-      console.log(err);
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
     });
+
+    newUser
+      .save()
+      .then(() => {
+        res.render("secrets");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
-  // check through the DB
   User.findOne({ email: username }).then(
     (foundUser) => {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          res.send("Wrong password");
-        }
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result == true) {
+            res.render("secrets");
+          } else {
+            res.send("Wrong password");
+          }
+        });
       } else {
         res.send("No such user");
       }
